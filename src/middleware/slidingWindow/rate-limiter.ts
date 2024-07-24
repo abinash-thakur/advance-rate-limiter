cc224a7038e0dd82ffbf11ab4d7324bfc98e65b4
Abinash Thakur <71590190+abinash-thakur@users.noreply.github.com>
2024-07-25 00:50:40 +0530

    modularize the code

---
+++ src/middleware/slidingWindow/rate-limiter.ts
@@ -0,0 +1,31 @@
+import slidingWindow from '../../utils/helper/sliding-window';
+import RateLimitOption from '../../utils/interface/ratelimitOption.interface';
+import { Request, Response, NextFunction } from 'express';
+import rateLimitConfigSchema from './rate-limiter.validator';
+import { NotAcceptableError } from '../../utils/errors/api-errors/NotAcceptableError';
+
+export default function rateLimiter(rateLimits: RateLimitOption) {
+    return async (req: Request, _res: Response, next: NextFunction) => {
+        try {
+            // Get the IP address from the request
+            const userIp = req.ip!;
+            const route = req.originalUrl.split('?')[0]; // Get the API endpoint
+            const rateLimitConfig = rateLimits[route];
+
+            //Joi Validation
+            const { error } = rateLimitConfigSchema.validate(rateLimitConfig);
+            if (error) {
+                throw new NotAcceptableError(`Invalid rate limit configuration for route: ${route} - ${error.message}`);
+            }
+
+            // If no rate limit is set for the route, skip the rate limiter
+            if (!rateLimitConfig) {
+                return next();
+            }
+
+            await slidingWindow(next, rateLimitConfig, userIp, route);
+        } catch (error) {
+            next(error);
+        }
+    };
+}
---
+++ src/utils/helper/sliding-window.ts
@@ -0,0 +1,54 @@
+import { NextFunction } from "express";
+import { AccessForbiddenError, InternalServerError } from "../errors";
+import { getRedisClient } from "../../config/redisClient";
+import { RateLimitConfig } from "../interface/ratelimit.interface";
+
+export default async function slidingWindow(next : NextFunction, rateLimitConfig : RateLimitConfig, rateLimitKey : string, route : string){
+    try{
+        const { limit, windowTime } = rateLimitConfig;
+        const client = getRedisClient();
+
+        const currentTime = Math.floor(Date.now() / 1000);
+        const currentWindowStart = Math.floor(currentTime / windowTime) * windowTime;
+        const previousWindowStart = currentWindowStart - windowTime;
+
+        const keys = [
+            `rateLimiterCurrent:${rateLimitKey}:${route}:${currentWindowStart}`,
+            `rateLimiterPrevious:${rateLimitKey}:${route}:${previousWindowStart}`,
+        ];
+
+        // Use a MULTI transaction
+        const multi = client.multi();
+
+        multi.incr(keys[0]);
+        multi.expire(keys[0], 2 * windowTime);
+        multi.get(keys[0]);
+        multi.get(keys[1]);
+
+        const rateLimitData = await multi.exec();
+        if (!rateLimitData) {
+            throw new InternalServerError('(RATELIMITER)::Some Error Occurs In RateLimiter!!');
+        }
+
+        const currentWindowCount = parseInt(String(rateLimitData[2]));
+        const prevWindowCount = parseInt(String(rateLimitData[3])) || 0;
+
+        if (currentWindowCount > limit) {
+            await client.set(keys[0], limit);
+            await client.expire(keys[0], 2 * windowTime);
+        }
+
+        const timeElapsedInCurrentWindow = currentTime - currentWindowStart;
+        const weightedCount =
+            ((windowTime - timeElapsedInCurrentWindow) / windowTime) * prevWindowCount + currentWindowCount;
+
+        if (weightedCount > limit) {
+            throw new AccessForbiddenError('(RATELIMITER)::Rate limit exceeded. Please try again later');
+        } else {
+            next();
+        }
+    }
+    catch(error){
+        next(error);
+    }
+}
